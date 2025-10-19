db648d9ef0b1e0a45d53148a340323e0f1507637
valueand <2426864343@qq.com>
2025-10-19 16:18:18 +0800

    Create 2_main.c

---
+++ 2_main.c
@@ -0,0 +1,23 @@
+//202310114112
+//751127773@qq.com
+//张博阳
+#include <stdio.h>
+
+int main() {
+    int score;
+    // 输入成绩
+    scanf("%d", &score);
+    // 从高分到低分判断等级
+    if (score >= 90 && score <= 100) {
+        printf("A\n");
+    } else if (score >= 80 && score <= 89) {
+        printf("B\n");
+    } else if (score >= 70 && score <= 79) {
+        printf("C\n");
+    } else if (score >= 60 && score <= 69) {
+        printf("D\n");
+    } else if (score >= 0 && score < 60) {
+        printf("E\n");
+    }
+    return 0;
+}
