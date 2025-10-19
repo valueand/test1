fd847bf275a74f2f464e8e5d5d62e1e0717af719
valueand <2426864343@qq.com>
2025-10-19 15:17:42 +0800

    Update 1_main.c

--- 1_main.c
+++ 1_main.c
@@ -1,9 +1,15 @@
-//
-//
-//
+//202310114112
+//751127773@qq.com
+//张博阳
 #include <stdio.h>
-int main()
+
+int main() 
 {
-    printf("Hello World");
+    float celsius  = 0;  // 已知输入的摄氏温度为30度
+    float fahrenheit;    // 用于存储转换后的华氏温度
+    scanf("%f",&celsius);
+    // 应用转换公式：华氏温度 = 摄氏温度 × 9/5 + 32
+    fahrenheit = celsius * 9 / 5 + 32;
+    printf("%.1f\n", fahrenheit);
     return 0;
 }
