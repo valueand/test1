e694b3ac0babdc04b50b0b7cd63ada15904082c1
valueand <2426864343@qq.com>
2025-10-19 15:58:14 +0800

    Create 3_main.c

---
+++ 3_main.c
@@ -0,0 +1,37 @@
+//202310114112
+//751127773@qq.com
+//张博阳
+#include <stdio.h>
+
+int main() 
+{
+    int a, b;
+    char op;  // 用于存储运算符
+
+    // 输入两个整数和运算符（格式如：3 5 +）
+    scanf("%d %d %c", &a, &b, &op);
+
+    // 根据运算符进行计算
+    if (op == '+') 
+    {
+        printf("%d\n", a + b);
+    } 
+    else if (op == '-') 
+    {
+        printf("%d\n", a - b);
+    }
+    else if (op == '*') 
+    {
+        printf("%d\n", a * b);
+    } 
+    else if (op == '/') 
+    {
+        
+          if (b != 0) 
+          {
+                  printf("%d\n", a / b);  
+          }
+       
+    }
+    return 0;
+}
