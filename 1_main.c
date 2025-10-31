f708ed464e2054e5a575747d489d1f46829d74e0
valueand <2426864343@qq.com>
2025-10-31 12:44:08 +0800

    Update 1_main.c

--- 1_main.c
+++ 1_main.c
@@ -1,40 +1,36 @@
-//202310114112
-//751127773@qq.com
-//张博阳
+//202410114214
+//2426864343@qq.com
+//李佳骏
 #include <stdio.h>
+
 int main()
 {
-    int n, is_prime = 1;
-    // 接收用户输入
-    scanf("%d", &n);
-    
-    // 判断是否为质数（小于2直接非质数）
-    if (n < 2) 
-    {
-        is_prime = 0;
-    } 
-    else
-    {
-        // 遍历2到n-1，判断是否能整除
-        for (int i = 2; i < n; i++)
-        {
-            if (n % i == 0) 
-            {
+    int n;
+    int is_prime = 1;
+
+    printf("请输入一个小于50的正整数作为密钥:");
+    if (scanf("%d", &n) != 1) return 0;
+
+    if (n <= 0 || n >= 50) {
+        printf("密钥不安全,请重新输入\n");
+        return 0;
+    }
+
+    if (n == 1) is_prime = 0;
+    else {
+        for (int i = 2; i <= n / 2; i++) {
+            if (n % i == 0) {
                 is_prime = 0;
                 break;
             }
         }
     }
-    
-    // 输出结果
-    if (is_prime) 
-    {
-        printf("密钥安全，密码设置成功\n");
+
+    if (is_prime) {
+        printf("密钥安全,密码设置成功\n");
+    } else {
+        printf("密钥不安全,请重新输入\n");
     }
-    else 
-    {
-        printf("密钥不安全，请重新输入\n");
-    }
-    
+
     return 0;
 }
