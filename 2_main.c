d8a3b85657601ae6bccf7224f27ece130ea62236
valueand <2426864343@qq.com>
2025-11-02 19:20:21 +0800

    Update 2_main.c

--- 2_main.c
+++ 2_main.c
@@ -11,12 +11,16 @@ int main() {
             scanf("%d", &matrix[i][j]);
         }
     }
-    // 输出转置矩阵（行列索引互换）
+    // 输出转置矩阵（控制行尾无多余空格，匹配示例格式）
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
-            printf("%d ", matrix[j][i]);
+            if (j == 0) {
+                printf("%d", matrix[j][i]); // 第一个元素无前置空格
+            } else {
+                printf(" %d", matrix[j][i]); // 后续元素前加空格
+            }
         }
-        printf("\n");
+        printf("\n"); // 每行结束换行
     }
     return 0;
 }
