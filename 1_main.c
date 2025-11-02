ffed278a6b7a89eb846dcab84ab33280871a0fd8
valueand <2426864343@qq.com>
2025-11-02 18:09:52 +0800

    Update 1_main.c

--- 1_main.c
+++ 1_main.c
@@ -2,17 +2,20 @@
 //751127773@qq.com
 //张博阳
 #include <stdio.h>
+
 int main() {
     int matrix[3][3];
     
-    // 读取3×3矩阵（输入可为"1 2 3\n4 5 6\n7 8 9"形式）
-    for (int i = 0; i < 3; i++) {
-        scanf("%d %d %d", &matrix[i][0], &matrix[i][1], &matrix[i][2]);
-    } 
-     
-    // 输出格式：每行元素用空格分隔，行与行之间用\n分隔，整体为一个连续字符串
-    // 第1行后加\n，第2行后加\n，第3行结尾加（匹配多余换行）
-    printf("%d %d %d\n%d %d %d\n%d %d %d", 
+    // 读取3×3矩阵：自动忽略输入中的空格和\n，按顺序读取9个整数
+    // 适配输入格式 "1 2 3\n4 5 6\n7 8 9"
+    scanf("%d %d %d %d %d %d %d %d %d",
+          &matrix[0][0], &matrix[0][1], &matrix[0][2],
+          &matrix[1][0], &matrix[1][1], &matrix[1][2],
+          &matrix[2][0], &matrix[2][1], &matrix[2][2]);
+    
+    // 原样输出：严格按 "元素1 元素2 元素3\n元素4...\n元素7..." 格式
+    // 输出结果与输入字符串完全一致，包含\n作为行分隔符
+    printf("%d %d %d\n%d %d %d\n%d %d %d",
            matrix[0][0], matrix[0][1], matrix[0][2],
            matrix[1][0], matrix[1][1], matrix[1][2],
            matrix[2][0], matrix[2][1], matrix[2][2]);
