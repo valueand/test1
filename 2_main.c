d8f29bce4193803ce614603dbc7d45c781f577ea
valueand <2426864343@qq.com>
2025-11-02 19:15:06 +0800

    Update 2_main.c

--- 2_main.c
+++ 2_main.c
@@ -5,33 +5,18 @@
 
 int main() {
     int matrix[3][3];
-    int transposed[3][3];
-    int i, j;
-
-    // 输入3x3矩阵
-    printf("请输入3x3矩阵（每行3个数字，用空格分隔）：\n");
-    for (i = 0; i < 3; i++) {
-        printf("第%d行：", i + 1);
-        for (j = 0; j < 3; j++) {
+    // 读取3×3矩阵
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
             scanf("%d", &matrix[i][j]);
         }
     }
-
-    // 计算转置矩阵：原矩阵的[i][j]对应转置矩阵的[j][i]
-    for (i = 0; i < 3; i++) {
-        for (j = 0; j < 3; j++) {
-            transposed[i][j] = matrix[j][i];
+    // 输出转置矩阵（行列索引互换）
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("%d ", matrix[j][i]);
         }
+        printf("\n");
     }
-
-    // 输出转置矩阵
-    printf("\n转置后的矩阵为：\n");
-    for (i = 0; i < 3; i++) {
-        for (j = 0; j < 3; j++) {
-            printf("%d ", transposed[i][j]);
-        }
-        printf("\n"); // 每行结束后换行
-    }
-
     return 0;
 }
