7f1f798b7c96599f4bddf6b0668cc22e87701c96
valueand <2426864343@qq.com>
2025-11-16 16:47:53 +0800

    Update 3_main.c

--- 3_main.c
+++ 3_main.c
@@ -3,36 +3,25 @@
 //张博阳
 #include <stdio.h>
 
+// 交换两个整数的函数，参数为指针
+void swap(int *a, int *b) {
+    int temp;  // 临时变量用于交换
+    temp = *a;  // 保存*a的值
+    *a = *b;    // 将*b的值赋给*a
+    *b = temp;  // 将临时保存的原*a值赋给*b
+}
+
 int main() {
-    int arr[10];
-    int i, j, temp;
-
-    // 输入10个数
-    printf("请输入10个整数：\n");
-    for (i = 0; i < 10; i++) {
-        scanf("%d", &arr[i]);
-    }
-
-    // 冒泡排序（从小到大）
-    // 外层循环控制排序轮数，共需要n-1轮（n为元素个数）
-    for (i = 0; i < 9; i++) {
-        // 内层循环控制每轮比较次数，每轮减少一次比较（已排好的元素无需再比较）
-        for (j = 0; j < 9 - i; j++) {
-            // 如果前一个数大于后一个数，则交换它们
-            if (arr[j] > arr[j + 1]) {
-                temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
-        }
-    }
-
-    // 输出排序结果
-    printf("排序后的结果为：\n");
-    for (i = 0; i < 10; i++) {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
-
+    int x, y;
+    
+    // 输入两个整数
+    scanf("%d %d", &x, &y);
+    
+    // 调用swap函数交换两个数（传递地址）
+    swap(&x, &y);
+    
+    // 打印交换后的结果
+    printf("%d %d\n", x, y);
+    
     return 0;
 }
