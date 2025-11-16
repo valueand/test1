f4f2b9dfb65097407e2cc773248e2cfa6664cce6
valueand <2426864343@qq.com>
2025-11-16 16:46:39 +0800

    Update 1_main.c

--- 1_main.c
+++ 1_main.c
@@ -4,21 +4,18 @@
 #include <stdio.h>
 
 int main() {
-    int matrix[3][3];
-    
-    // 读取3×3矩阵：自动忽略输入中的空格和\n，按顺序读取9个整数
-    // 适配输入格式 "1 2 3\n4 5 6\n7 8 9"
-    scanf("%d %d %d %d %d %d %d %d %d",
-          &matrix[0][0], &matrix[0][1], &matrix[0][2],
-          &matrix[1][0], &matrix[1][1], &matrix[1][2],
-          &matrix[2][0], &matrix[2][1], &matrix[2][2]);
-    
-    // 原样输出：严格按 "元素1 元素2 元素3\n元素4...\n元素7..." 格式
-    // 输出结果与输入字符串完全一致，包含\n作为行分隔符
-    printf("%d %d %d\n%d %d %d\n%d %d %d",
-           matrix[0][0], matrix[0][1], matrix[0][2],
-           matrix[1][0], matrix[1][1], matrix[1][2],
-           matrix[2][0], matrix[2][1], matrix[2][2]);
-    
+    int a;         // 定义整数变量a
+    int *p = &a;   // 定义指针p，指向a
+
+    // 输入a的初始值
+    scanf("%d", &a);
+
+    // 通过指针修改a的值（这里示例改为初始值加10，也可改为其他逻辑）
+    *p = *p + 10;  // 等价于 a = a + 10
+
+    // 打印结果
+    printf("%d\n", a);
+    printf("%d\n", *p);
+
     return 0;
 }
